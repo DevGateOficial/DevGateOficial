1ec966e366147ffe8a4d60e057f12428303e7a20
Brambs <gabriel128.br@gmail.com>
2023-01-29 20:13:34 -0300

    About small tweak

--- App/Public/Views/sobre/sobre.php
+++ App/Public/Views/sobre/sobre.php
@@ -47,96 +47,100 @@
       </ul>
     </div>
   </div>
-  <div class="tec-tools">
-    <h2 class="tec-titulo">Tecnologias utilizadas</h2>
-    <div class="tec-imgs-wrap">
-      <a href="https://www.php.net" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/php-logo.svg" alt="logo php" class="tec-img spec" />
-      </a>
-      <a href="https://html5.org" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/logo-html-5-.png" alt="logo html" class="tec-img" />
-      </a>
-      <a href="https://www.w3.org/Style/CSS/" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/logo-css.png" alt="logo css" class="tec-img" />
-      </a>
-      <a href="https://sass-lang.com" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/Sass-Logo.svg.png" alt="logo scss" class="tec-img spec" />
-      </a>
-      <a href="https://www.javascript.com/" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/javascript-logo-1.png" alt="logo javascript" class="tec-img" />
-      </a>
-      <a href=" https://www.mysql.com/" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/MySQL-Logo.wine.png" alt="logo mysql" class="tec-img" />
-      </a>
-      <a href="https://git-scm.com" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/Git-Icon.png" alt="logo git" class="tec-img spec" />
-      </a>
-      <a href="http://api.jquery.com/jquery.ajax/" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/ajax.png" alt="logo ajax js assíncrono" class="tec-img spec" />
-      </a>
-      <a href="https://github.com/PHPMailer/PHPMailer" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/php-mailer.png" alt="logo php mailer" class="tec-img" />
-      </a>
+  <div>
+
+    <div class="tec-tools">
+      <h2 class="tec-titulo">Tecnologias utilizadas</h2>
+      <div class="tec-imgs-wrap">
+        <a href="https://www.php.net" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/php-logo.svg" alt="logo php" class="tec-img spec" />
+        </a>
+        <a href="https://html5.org" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/logo-html-5-.png" alt="logo html" class="tec-img" />
+        </a>
+        <a href="https://www.w3.org/Style/CSS/" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/logo-css.png" alt="logo css" class="tec-img" />
+        </a>
+        <a href="https://sass-lang.com" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/Sass-Logo.svg.png" alt="logo scss" class="tec-img spec" />
+        </a>
+        <a href="https://www.javascript.com/" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/javascript-logo-1.png" alt="logo javascript" class="tec-img" />
+        </a>
+        <a href=" https://www.mysql.com/" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/MySQL-Logo.wine.png" alt="logo mysql" class="tec-img" />
+        </a>
+        <a href="https://git-scm.com" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/Git-Icon.png" alt="logo git" class="tec-img spec" />
+        </a>
+        <a href="http://api.jquery.com/jquery.ajax/" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/ajax.png" alt="logo ajax js assíncrono" class="tec-img spec" />
+        </a>
+        <a href="https://github.com/PHPMailer/PHPMailer" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/php-mailer.png" alt="logo php mailer" class="tec-img" />
+        </a>
+      </div>
+    </div>
+    <div class="tec-tools spacer-fix">
+      <h2 class="tec-titulo">Ferramentas utilizadas</h2>
+      <div class="tec-imgs-wrap">
+        <a href="https://www.google.com/intl/pt-BR/drive/" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/logo-driver.png" alt="logo" class="tec-img spec" />
+        </a>
+        <a href="https://www.notion.so" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/Notion-logo.png" alt="logo" class="tec-img spec" />
+        </a>
+        <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/logo-discord.png" alt="logo" class="tec-img spec" />
+        </a>
+        <a href="https://desktop.github.com" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/Github-desktop-logo.svg.png" alt="logo" class="tec-img spec" />
+        </a>
+        <a href="https://code.visualstudio.com" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/Visual-Studio-icon.svg.png" alt="logo" class="tec-img spec" />
+        </a>
+        <a href="https://www.figma.com" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/Figma-logo.svg" alt="logo" class="tec-img spec" />
+        </a>
+        <a href="https://www.wampserver.com/" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/WampServer-logo.png" alt="logo" class="tec-img spec" />
+        </a>
+        <a href="https://online.visual-paradigm.com/pt/login.jsp" target="_blank" rel="noopener noreferrer">
+          <img src="<?= URLSRC ?>assets/img/sobre/vp-online-logo.png" alt="logo" class="tec-img spec" />
+        </a>
+      </div>
     </div>
   </div>
-  <div class="tec-tools spacer-fix">
-    <h2 class="tec-titulo">Ferramentas utilizadas</h2>
-    <div class="tec-imgs-wrap">
-      <a href="https://www.google.com/intl/pt-BR/drive/" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/logo-driver.png" alt="logo" class="tec-img spec" />
-      </a>
-      <a href="https://www.notion.so" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/Notion-logo.png" alt="logo" class="tec-img spec" />
-      </a>
-      <a href="https://discord.com" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/logo-discord.png" alt="logo" class="tec-img spec" />
-      </a>
-      <a href="https://desktop.github.com" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/Github-desktop-logo.svg.png" alt="logo" class="tec-img spec" />
-      </a>
-      <a href="https://code.visualstudio.com" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/Visual-Studio-icon.svg.png" alt="logo" class="tec-img spec" />
-      </a>
-      <a href="https://www.figma.com" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/Figma-logo.svg" alt="logo" class="tec-img spec" />
-      </a>
-      <a href="https://www.wampserver.com/" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/WampServer-logo.png" alt="logo" class="tec-img spec" />
-      </a>
-      <a href="https://online.visual-paradigm.com/pt/login.jsp" target="_blank" rel="noopener noreferrer">
-        <img src="<?=URLSRC?>assets/img/sobre/vp-online-logo.png" alt="logo" class="tec-img spec" />
-      </a>
-    </div>
-  </div>
+
   <h2 class="dev-titulo">Equipe de desenvolvedores</h2>
   <div class="desenvolvedores">
     <div class="dev">
-      <img class="dev-1" src="<?=URLSRC?>assets/img/sobre/daniel.jfif" alt="" />
+      <img class="dev-1" src="<?= URLSRC ?>assets/img/sobre/daniel.jfif" alt="" />
       <h2>Daniel Chielle</h2>
       <p>Desenvolvedor - Back end</p>
       <div class="git">
         <a href="https://github.com/LeinadRPFI" target="_blank" rel="noopener noreferrer">
-          <img src="<?=URLSRC?>assets/img/adm-icon.png" alt="" /> GitHub
+          <img src="<?= URLSRC ?>assets/img/adm-icon.png" alt="" /> GitHub
         </a>
       </div>
     </div>
     <div class="dev">
-      <img class="dev-2" src="<?=URLSRC?>assets/img/sobre/felipe.png" alt="" />
+      <img class="dev-2" src="<?= URLSRC ?>assets/img/sobre/felipe.png" alt="" />
       <h2>Felipe Gonçalves Dias</h2>
       <p>Desenvolvedor - Back end</p>
       <div class="git">
         <a href="https://github.com/FelipeSGoncalves" target="_blank" rel="noopener noreferrer">
-          <img src="<?=URLSRC?>assets/img/adm-icon.png" alt="" /> GitHub
+          <img src="<?= URLSRC ?>assets/img/adm-icon.png" alt="" /> GitHub
         </a>
       </div>
     </div>
     <div class="dev">
-      <img class="dev-3" src="<?=URLSRC?>assets/img/sobre/brambillajpeg.jpeg" alt="" />
+      <img class="dev-3" src="<?= URLSRC ?>assets/img/sobre/brambillajpeg.jpeg" alt="" />
       <h2>Gabriel Eduardo Brambilla</h2>
       <p>Desenvolvedor - Front end</p>
       <div class="git">
         <a href="https://github.com/GabrielEduardoBrambilla" target="_blank" rel="noopener noreferrer">
-          <img src="<?=URLSRC?>assets/img/adm-icon.png" alt="" /> GitHub
+          <img src="<?= URLSRC ?>assets/img/adm-icon.png" alt="" /> GitHub
         </a>
       </div>
     </div>
